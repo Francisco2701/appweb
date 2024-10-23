20ae5b8ed1dfb6c14cef1ef84d55296cfbd79f56
FranciscoGabriel <franciscogabriel.ma40788@cesurformacion.com>
2024-10-23 13:09:23 +0200

    s

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) { 
-  res.render('home.ejs', {head_title: "Principal"});
+  res.render('home', {head_title: "Principal"});
 });
 
 
