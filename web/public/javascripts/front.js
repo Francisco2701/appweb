93869dba779a69ea3fa165d07957dcc64640ddcf
FranciscoGabriel <franciscogabriel.ma40788@cesurformacion.com>
2024-10-23 12:16:34 +0200

    blbalblbalbla

--- web/public/javascripts/front.js
+++ web/public/javascripts/front.js
@@ -1,1 +1,24 @@
 console.log("hola mundo...")
+
+document.querySelector("input#lenguaje").value = navigator.userAgent;
+
+document.querySelector("a#open-offcanvas").addEventListener("click", ()=>{
+    document.querySelector("aside#offcanvas").classList.toggle("show");
+});
+
+const button_close = document.querySelector("#offcanvas button.close");
+if (button_close){
+    button_close.addEventListener("click", ()=>{
+        document.querySelector("aside#offcanvas").classList.remove("show");
+    });
+}
+
+document.querySelectorAll("#galeria .inner figure").forEach( (el)=>{
+    el.addEventListener("clicl", (event)=>{
+        const fig = el;
+        console.log(fig);
+        const image = fig.querySelector("img").src;
+        document.querySelector("dialog#lightbox img").src = image;
+        document.querySelector("dialog#lightbox").showModal();
+    })
+})
--- web/routes/index.js
+++ web/routes/index.js
@@ -2,8 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res, next) { 
+  res.render('home', {head_title: "Principal"});
 });
+
+
+router.get('/celebraciones', function(req, res, next) {
+  res.render("celebraciones", {head_title: "celebraciones"});
+});
+
+
+router.get('/contacta-con-nosotros', function(req, res, next) {
+  res.render("contacto", {head_title: "contacto"});
+});
+
+router.get('/contact-with-us', function(req, res, next) {
+  res.render("contacto", {head_title: "contacto"});
+});
+
+router.get('/galeria', function(req, res, next) {
+  res.render("galeria", {head_title: "galeria"});
+});
+
+router.get('/debug/:category/:id', function(req, res, next) {
+  console.log(req.body);
+  console.log(req.hostname);
+  console.log(req.params);
+  console.log(req.query);
+  res.send("ok");
+});
+
 
 module.exports = router;
